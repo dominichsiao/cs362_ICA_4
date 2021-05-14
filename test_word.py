12fc94ccfc715dc940782fb5cd82b1fe5ba69d30
Dominic Hsiao <hsiaod@oregonstate.edu>
2021-05-13 17:20:54 -0700

    second commit

--- test_word.py
+++ test_word.py
@@ -7,8 +7,11 @@ class testCaseAdd(unittest.TestCase):
 	def test_word_2(self):
 		self.assertEqual(word_count.count("I"), 1)
 
-	def test_sub(self):
+	def test_word_3(self):
 		self.assertEqual(word_count.count(""),0)
+	
+	def test_word_4(self):
+		self.assertEqual(word_count.count("Hello,There"), 2)
 
 #if __name__ = "__main__":
 	#unittest.main()
